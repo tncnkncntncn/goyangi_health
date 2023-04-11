64db5eb239653ee0615771afe62243041ed5f792
渡辺友美 <tncnkncn.0329@gmail.com>
2023-04-11 10:45:18 +0900

    device導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  #before_action :basic_auth
 
 
 
 
 
-  private
+  #private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER2"] && password == ENV["BASIC_AUTH_PASSWORD2"]     end
-  end
+  #def basic_auth
+    #authenticate_or_request_with_http_basic do |username, password|
+      #username == ENV["BASIC_AUTH_USER2"] && password == ENV["BASIC_AUTH_PASSWORD2"]     end
+  #end
 end
-
