79fbe66b7ad37f6c55933f366faa65698c94d67f
渡辺友美 <tncnkncn.0329@gmail.com>
2023-04-11 14:06:16 +0900

    トップページの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
-  #before_action :basic_auth
+  before_action :basic_auth
 
 
 
 
 
-  #private
+private
 
-  #def basic_auth
-    #authenticate_or_request_with_http_basic do |username, password|
-      #username == ENV["BASIC_AUTH_USER2"] && password == ENV["BASIC_AUTH_PASSWORD2"]     end
-  #end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  
+  end   
+end
 end
