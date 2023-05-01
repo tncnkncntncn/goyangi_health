c9ad854b397c69d7a76b33ea9f3303715bb50c3a
渡辺友美 <tncnkncn.0329@gmail.com>
2023-05-01 23:54:32 +0900

    userモデルバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は英字と数字を含む６文字以上の半角英数字で入力してください" }
+  validates :nickname,:name, format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message:"全角で入力してください" }
+  validates :nick_name, presence: true
   has_many :cats
   has_many :records
+
+  
 end
